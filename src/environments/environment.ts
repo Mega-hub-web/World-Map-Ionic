593c7050612f4f57729e181879932ec3a0e6bd17
Mega-hub-web <evander.calloway@proton.me>
2025-04-30 07:13:24 -0400

    fix the backend address

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
-export const environment = {
-  production: false,
-  apiUrl: "http://localhost:5001/api", // Replace with your backend server URL
-};
 // export const environment = {
 //   production: false,
-//   apiUrl: "https://worldmap-backend.onrender.com/api", // Replace with your backend server URL
+//   apiUrl: "http://localhost:5001/api", // Replace with your backend server URL
 // };
+export const environment = {
+  production: false,
+  apiUrl: "https://worldmap-backend.onrender.com/api", // Replace with your backend server URL
+};
